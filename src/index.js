34b32590bd03220be768399a7b08dd4bc267b2dd
byoon97 <byoon5397@gmail.com>
2023-03-22 15:05:27 -0400

    testing replies

---
+++ src/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config(); //initialize dotenv
+const { Client, IntentsBitField } = require("discord.js");
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) {
+    return;
+  }
+
+  if (message.content === "hello") {
+    message.reply("hello");
+  }
+});
+
+client.login(process.env.CLIENT_TOKEN); //login bot using token
