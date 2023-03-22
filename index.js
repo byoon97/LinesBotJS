673f2d951f3564fbf706be1be8f8ce83f9ae888b
byoon97 <byoon5397@gmail.com>
2023-03-22 13:53:41 -0400

    able to greet bot

--- index.js
+++ index.js
@@ -7,5 +7,11 @@ client.once(Events.ClientReady, (c) => {
   console.log(`Logged in as ${c.user.tag}`);
 });
 
+client.on("message", (message) => {
+  if (message.content === "!greetings") {
+    message.channel.send("hello");
+  }
+});
+
 //make sure this line is the last line
 client.login(process.env.CLIENT_TOKEN); //login bot using token
